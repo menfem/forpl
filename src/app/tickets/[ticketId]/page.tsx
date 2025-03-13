4bdfb5d41a84d71edd0044fed0f11aa5a58872b2
menfem <connor.royes@me.com>
2025-03-13 16:31:45 +0000

    latest commit

---
+++ src/app/tickets/[ticketId]/page.tsx
@@ -0,0 +1,24 @@
+import { initialTickets } from '@/data'
+
+type TicketPageProps = {
+  params: {
+    ticketId: string
+  }
+}
+
+const TicketPage = ({ params }: TicketPageProps) => {
+  const ticket = initialTickets.find((ticket) => ticket.id === params.ticketId)
+
+  if (!ticket) {
+    return <div>Ticket not found</div>
+  }
+
+  return (
+    <div>
+      <h2>{ticket.title}</h2>
+      <p>{ticket.content}</p>
+    </div>
+  )
+}
+
+export default TicketPage
---
+++ src/paths.ts
@@ -0,0 +1,5 @@
+export const ticketsPath = () => '/tickets'
+
+export const ticketPath = (ticketId: string) => `/tickets/${ticketId}`
+
+export const homePath = () => '/'
