904c55932520aa9990aedca439e7efe1c629a28d
menfem <connor.royes@me.com>
2025-03-13 19:21:10 +0000

    added ticket icons

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,11 +2,17 @@ import Link from 'next/link'
 import { initialTickets } from '@/data'
 import { ticketsPath } from '@/paths'
 
+const TICKET_ICONS = {
+  OPEN: 'O',
+  DONE: 'X',
+}
+
 const TicketsPage = () => {
   return (
     <div>
       {initialTickets.map((ticket) => (
         <div key={ticket.id}>
+          <div>{TICKET_ICONS[ticket.status]}</div>
           <h2 className="text-lg">{ticket.title}</h2>
           <Link href={ticketsPath()} className="text-sm underline">
             View
--- src/data.ts
+++ src/data.ts
@@ -3,18 +3,18 @@ export const initialTickets = [
     id: '1',
     title: 'ticket 1',
     content: 'This is the first ticket',
-    status: 'DONE',
+    status: 'DONE' as const,
   },
   {
     id: '2',
     title: 'ticket 2',
     content: 'This is the second ticket',
-    status: 'OPEN',
+    status: 'OPEN' as const,
   },
   {
     id: '3',
     title: 'ticket 3',
     content: 'This is the third ticket',
-    status: 'DoONE',
+    status: 'DONE' as const,
   },
 ]
